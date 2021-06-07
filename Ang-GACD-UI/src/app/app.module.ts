36a3ad0b1b5fa12ce92911d5975234f1e51d7f77
CWeafer-D <83568313+CWeafer-D@users.noreply.github.com>
2021-06-07 14:18:23 -0700

    rescaffold pt2

--- Ang-GACD-UI/src/app/app.module.ts
+++ Ang-GACD-UI/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmpInComponent } from './emp-in/emp-in.component';
-import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmpInComponent,
-    UserInfoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
