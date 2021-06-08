c93a49f7f17a043aa390cc819c4a3e6ab73a658c
Garrett Ruffner <garrettruffner42@gmail.com>
2021-06-08 13:57:49 -0400

    Revert "Auth0 added"

--- Ang-GACD-UI/src/environments/environment.ts
+++ Ang-GACD-UI/src/environments/environment.ts
@@ -1,16 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import authSettings from '../../auth_config.json' ;
 
 export const environment = {
-  production: false,
-  auth: {
-    domain: authSettings.domain,
-    clientId: authSettings.clientId,
-   
-    redirectUri: window.location.origin,
-  },
+  production: false
 };
 
 /*
