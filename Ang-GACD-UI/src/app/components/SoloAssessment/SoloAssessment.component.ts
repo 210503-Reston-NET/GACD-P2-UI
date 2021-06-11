4dca775ba496f9f6445a2b0b4c1da4c4372d4c49
phobos42 <garrettruffner42@gmail.com>
2021-06-11 16:48:41 -0400

    Fixed up dougs code

--- Ang-GACD-UI/src/app/components/SoloAssessment/SoloAssessment.component.ts
+++ Ang-GACD-UI/src/app/components/SoloAssessment/SoloAssessment.component.ts
@@ -1,126 +1,126 @@
-import { Component } from '@angular/core';
-import { Usermodel } from 'src/Models/UserModel';
-import {UserService} from 'src/Services/User.service';
-import { AppComponent } from 'src/app/app.component';
+// import { Component } from '@angular/core';
+// import { Usermodel } from 'src/Models/UserModel';
+// import {UserService} from 'src/Services/User.service';
+// import { AppComponent } from 'src/app/app.component';
 
-// Time Limit
-let timeLimit: number;
-//place for category
+// // Time Limit
+// let timeLimit: number;
+// //place for category
 
-let testCategory = "Text";
-if(testCategory === "Text"){
-timeLimit = 60;
-}else{
-    timeLimit = 120;
-}
-// selecting required elements
-let timer_text = document.getElementById(".timerValue") as HTMLDivElement;
-let error_text = document.getElementById(".errorsValue") as HTMLDivElement;
-let charactersTyped_text = document.getElementById("charactersTypedValue") as HTMLDivElement; 
-let quote_text = document.getElementById("snippet_area") as HTMLDivElement;
-let input_area = document.getElementById(".entry_area") as HTMLTextAreaElement;
-//let restart_btn = document.getElementById(".restart_btn") as HTMLButtonElement;
-//let chracters_group = document.getElementById(".typeCount") as HTMLAnchorElement;
-//let erraor_group = document.getElementById(".error") as HTMLAnchorElement;
-let testString:string;
-let timeRemaining = timeLimit;
-let timeElapsed = 0;
-let total_errors = 0;
-let errors = 0;
-let characterTyped = 0;
-let current_quote = "";
-let quoteNo = 0;
-let timer: NodeJS.Timeout;
+// let testCategory = "Text";
+// if(testCategory === "Text"){
+// timeLimit = 60;
+// }else{
+//     timeLimit = 120;
+// }
+// // selecting required elements
+// let timer_text = document.getElementById(".timerValue") as HTMLDivElement;
+// let error_text = document.getElementById(".errorsValue") as HTMLDivElement;
+// let charactersTyped_text = document.getElementById("charactersTypedValue") as HTMLDivElement; 
+// let quote_text = document.getElementById("snippet_area") as HTMLDivElement;
+// let input_area = document.getElementById(".entry_area") as HTMLTextAreaElement;
+// //let restart_btn = document.getElementById(".restart_btn") as HTMLButtonElement;
+// //let chracters_group = document.getElementById(".typeCount") as HTMLAnchorElement;
+// //let erraor_group = document.getElementById(".error") as HTMLAnchorElement;
+// let testString:string;
+// let timeRemaining = timeLimit;
+// let timeElapsed = 0;
+// let total_errors = 0;
+// let errors = 0;
+// let characterTyped = 0;
+// let current_quote = "";
+// let quoteNo = 0;
+// let timer: NodeJS.Timeout;
 
 
-function Start(){
-testString = "Since fours veiling shadow Minas Morgul crawl. Finer back falls filth Pippin's guardian!"
-timer_text.textContent = timeRemaining.toString();
-}
-function ResetGame(){
-    timeElapsed = 0;
-    errors = 0;
-    characterTyped = 0;
-    testString = "";
-}
-function ProcessText(){
-    //get the current input and split
-    let currentInput: string = input_area.value;
-    let typedChars :string[]  = currentInput.split("");
+// function Start(){
+// testString = "Since fours veiling shadow Minas Morgul crawl. Finer back falls filth Pippin's guardian!"
+// timer_text.textContent = timeRemaining.toString();
+// }
+// function ResetGame(){
+//     timeElapsed = 0;
+//     errors = 0;
+//     characterTyped = 0;
+//     testString = "";
+// }
+// function ProcessText(){
+//     //get the current input and split
+//     let currentInput: string = input_area.value;
+//     let typedChars :string[]  = currentInput.split("");
     
-    // increment total characters typed
-    characterTyped++;
+//     // increment total characters typed
+//     characterTyped++;
     
-    errors = 0;
+//     errors = 0;
     
-    let quote:String = quote_text.textContent;
-    let charsToType= quote.split("")
-    charsToType.forEach((char, index) => {
-      let typedChar = typedChars[index]
-      clearInterval(timer);
-      timer = setInterval(RunTimer, 1000);
+//     let quote:String = quote_text.textContent;
+//     let charsToType= quote.split("")
+//     charsToType.forEach((char, index) => {
+//       let typedChar = typedChars[index]
+//       clearInterval(timer);
+//       timer = setInterval(RunTimer, 1000);
       
-      if (typedChar == null) {
-        //set character color to normal character color from stylesheet
-      } else if (typedChar != char) {
-        //set character color to red "#8B0000";
+//       if (typedChar == null) {
+//         //set character color to normal character color from stylesheet
+//       } else if (typedChar != char) {
+//         //set character color to red "#8B0000";
     
-        // increment number of errors
-        errors++;
-      }
-    });
+//         // increment number of errors
+//         errors++;
+//       }
+//     });
     
-    // display the number of errors
-    //if(error_text != null)
-    {
-     //   error_text.value = errors.toString();
-    }
+//     // display the number of errors
+//     //if(error_text != null)
+//     {
+//      //   error_text.value = errors.toString();
+//     }
     
-    // update accuracy text
+//     // update accuracy text
     
     
-    // irrespective of errors
+//     // irrespective of errors
   
-      // update total errors
-      total_errors = errors;
+//       // update total errors
+//       total_errors = errors;
     
-      // clear the input area
-}
-function finishGame() {
-  clearInterval(timer);
-   input_area.readOnly = true
+//       // clear the input area
+// }
+// function finishGame() {
+//   clearInterval(timer);
+//    input_area.readOnly = true
    
-   //send data to backend to create new tests object
-            //send userstatsId, errors, wordsTyped and timeTaken
-            //get todays date and send that
-  }
-  function RunTimer() {
-    if(timeRemaining > 0){
-        timeRemaining--;
-        timeElapsed++;
-        timer_text.textContent = timeRemaining.toString();
-    } 
-    else{
-        finishGame();
-    }
+//    //send data to backend to create new tests object
+//             //send userstatsId, errors, wordsTyped and timeTaken
+//             //get todays date and send that
+//   }
+//   function RunTimer() {
+//     if(timeRemaining > 0){
+//         timeRemaining--;
+//         timeElapsed++;
+//         timer_text.textContent = timeRemaining.toString();
+//     } 
+//     else{
+//         finishGame();
+//     }
 
-  }
+//   }
   
-  function startGame() {
+//   function startGame() {
   
-    resetValues();
+//     resetValues();
   
-    // clear old and start a new timer
-    clearInterval(timer);
-    setInterval(RunTimer, 1000);
-  }
+//     // clear old and start a new timer
+//     clearInterval(timer);
+//     setInterval(RunTimer, 1000);
+//   }
   
-  function resetValues() {
-    timeRemaining = timeLimit;
-    timeElapsed = 0;
-    errors = 0;
-    total_errors = 0;
-    characterTyped = 0;
-        //reset the value of the input area
-    //reset the value of the quote area
-  }
+//   function resetValues() {
+//     timeRemaining = timeLimit;
+//     timeElapsed = 0;
+//     errors = 0;
+//     total_errors = 0;
+//     characterTyped = 0;
+//         //reset the value of the input area
+//     //reset the value of the quote area
+//   }
--- Ang-GACD-UI/src/app/pages/test/test.component.ts
+++ Ang-GACD-UI/src/app/pages/test/test.component.ts
@@ -22,7 +22,7 @@ export class TestComponent implements OnInit {
 
   ngOnInit(): void{
     //place for category
-    this.Start();
+    this.testString = "Hello world!"
     let testCategory = "Text";
     if(testCategory === "Text"){
     this.timeLimit = 60;
@@ -35,17 +35,17 @@ export class TestComponent implements OnInit {
   // Time Limit
  
   // selecting required elements
-  timer_text = document.getElementById(".timerValue") as HTMLDivElement;
-  error_text = document.getElementById(".errorsValue") as HTMLDivElement;
-  charactersTyped_text = document.getElementById("charactersTypedValue") as HTMLDivElement; 
-  quote_text = document.getElementById("snippet_area") as HTMLDivElement;
+  // timer_text = document.getElementById(".timerValue") as HTMLDivElement;
+  // error_text = document.getElementById(".errorsValue") as HTMLDivElement;
+  // charactersTyped_text = document.getElementById("charactersTypedValue") as HTMLDivElement; 
+  //quote_text = document.getElementById("snippet_area") as HTMLDivElement;
   input_area = document.getElementById(".entry_area") as HTMLTextAreaElement;
   //let restart_btn = document.getElementById(".restart_btn") as HTMLButtonElement;
   //let chracters_group = document.getElementById(".typeCount") as HTMLAnchorElement;
   //let erraor_group = document.getElementById(".error") as HTMLAnchorElement;
   testString:string;
-  timeLimit: number;
-  timeRemaining: number;
+  timeLimit: number = 0;
+  timeRemaining: number = 0;
   timeElapsed: number = 0;
   total_errors: number = 0;
   errors: number = 0;
@@ -53,34 +53,50 @@ export class TestComponent implements OnInit {
   current_quote: string = "";
   quoteNo: number= 0;
   timer: NodeJS.Timeout;
+  currentInput: string;
+  started: boolean = false;
 
 
-  Start(): void{
-    this.testString = "Since fours veiling shadow Minas Morgul crawl. Finer back falls filth Pippin's guardian!"
-    this.timer_text.textContent = this.timeRemaining.toString();
-  }
   ResetGame(): void{
       this.timeElapsed = 0;
       this.errors = 0;
       this.characterTyped = 0;
       this.testString = "";
   }
+
+
   ProcessText(): void{
       //get the current input and split
-      let currentInput: string = this.input_area.value;
-      let typedChars :string[]  = currentInput.split("");
+      // let currentInput: string = this.input_area.value;
+      if(!this.started){
+        //console.log(this.timeRemaining)
+        this.started = true;
+        this.startGame();
+      }
+      
+      let typedChars :string[]  = this.currentInput.split("");
       
       // increment total characters typed
       this.characterTyped++;
       
       this.errors = 0;
       
-      let quote:String = this.quote_text.textContent;
+      let quote:String = this.testString;
       let charsToType= quote.split("")
       charsToType.forEach((char, index) => {
         let typedChar = typedChars[index]
         clearInterval(this.timer);
-        this.timer = setInterval(this.RunTimer, 1000);
+        this.timer = setInterval(() => 
+        {
+          console.log(this.timeRemaining)
+            if(this.timeRemaining > 0){
+              this.timeRemaining--;
+              this.timeElapsed++;
+            }else{
+              clearInterval(this.timer)
+              this.input_area.readOnly = true
+            }
+          }, 1000);
         
         if (typedChar == null) {
           //set character color to normal character color from stylesheet
@@ -90,62 +106,46 @@ export class TestComponent implements OnInit {
           // increment number of errors
           this.errors++;
         }
-      });
-      
-      // display the number of errors
-      //if(error_text != null)
-      {
-      //   error_text.value = errors.toString();
-      }
-      
-      // update accuracy text
-      
-      
-      // irrespective of errors
-    
-        // update total errors
-        this.total_errors = this.errors;
-      
-        // clear the input area
+      });  
+      this.total_errors = this.errors;
   }
-  finishGame() {
+  startGame() { 
+      
+    //this.resetValues();    
+    // clear old and start a new timer
     clearInterval(this.timer);
-    this.input_area.readOnly = true
+    //setInterval(this.RunTimer, 1000);
+    //console.log(this.timeRemaining)
+  }
     
-    //send data to backend to create new tests object
-              //send userstatsId, errors, wordsTyped and timeTaken
-              //get todays date and send that
-    }
-
-    RunTimer() {
-      if(this.timeRemaining > 0){
-          this.timeRemaining--;
-          this.timeElapsed++;
-          this.timer_text.textContent = this.timeRemaining.toString();
-      } 
-      else{
-          this.finishGame();
-      }
-    }
-    
-    startGame() {    
-      this.resetValues();    
-      // clear old and start a new timer
-      clearInterval(this.timer);
-      setInterval(this.RunTimer, 1000);
-    }
-    
-    resetValues() {
-      this.timeRemaining = this.timeLimit;
-      this.timeElapsed = 0;
-      this.errors = 0;
-      this.total_errors = 0;
-      this.characterTyped = 0;
-          //reset the value of the input area
-      //reset the value of the quote area
-    }
+  resetValues() {
+    this.timeRemaining = this.timeLimit;
+    this.timeElapsed = 0;
+    this.errors = 0;
+    this.total_errors = 0;
+    this.characterTyped = 0;
+        //reset the value of the input area
+    //reset the value of the quote area
+  }
 
 }
+
+
+  // FinishGame(): void{
+  //   clearInterval(this.timer)
+  //   this.input_area.readOnly = true
+  // }
+
+  // RunTimer(): void{
+  //   console.log(this.timeRemaining)
+  //   if(this.timeRemaining > 0){
+  //     this.timeRemaining--;
+  //     this.timeElapsed++;
+  //      // this.timeRemaining
+  //   }else{
+  //     this.FinishGame();
+  //   }
+  // }
 
 
 
