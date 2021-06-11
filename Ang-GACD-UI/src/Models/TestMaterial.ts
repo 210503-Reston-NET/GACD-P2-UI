c34a2002d2b6c631cc40557c7f9e9302d65ba10a
phobos42 <garrettruffner42@gmail.com>
2021-06-10 20:41:30 -0400

    Added full test material test button

--- Ang-GACD-UI/src/Models/TestMaterial.ts
+++ Ang-GACD-UI/src/Models/TestMaterial.ts
@@ -1,6 +1,6 @@
 import { Language } from "./LanguageEnum";
 export class TestMaterial{
-    catagory: Language;
+    catagoryId: Language;
     content: string;
     author: string;
     length: number;
