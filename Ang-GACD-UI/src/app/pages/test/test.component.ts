ccfe450bcf3441a4bc81973b8798f5b201fa28b1
phobos42 <garrettruffner42@gmail.com>
2021-06-12 04:02:24 -0400

    Add ability to submit model

--- Ang-GACD-UI/src/app/pages/test/test.component.ts
+++ Ang-GACD-UI/src/app/pages/test/test.component.ts
@@ -178,82 +178,18 @@ export class TestComponent implements OnInit {
     }
   }
 
+  submitResults(){
+    let model: TestModel = {
+      catagoryId: this.testmat.catagoryId,
+      numberofcharacters : this.testmat.content.length,
+      numberoferrors: this.state.errors,
+      timetakenms : this.timeTaken,
+      date: new Date()
+    }
+    this.api.postTestResults(model);
+  }
+
 }
 
 
 
-
-
-  //Garretts code
-  // ngOnInit(): void {
-  //   this.newTest();
-  //   // this.state.words = this.state.typeTest.split(' ')
-  //   // this.api.getTestContentByCatagoryId(-1).then(
-  //   //   (obj)=> {this.testmat = obj
-  //   //   this.state.words = this.testmat.content.split(' ');
-  //   // })
-
-  // }
-
-  // newTest(): void{
-  //   this.state.wordsPerMinute = null;
-  //   this.state.words =[]
-  //   this.state.enteredText= ''
-  //   this.state.correctCount= 0
-  //   this.state.started= false
-  //   this.state.startTime= null
-  //   this.state.wordsPerMinute = null
-
-  //   this.api.getTestContentByCatagoryId(-1).then(
-  //     (obj)=> {this.testmat = obj
-  //     this.state.words = this.testmat.content.split(' ');
-  //   })
-
-  // }
-
-  // testmat: TestMaterial = null;
-
-  // state: State = {
-  //   words: [],
-  //   enteredText: '',
-  //   correctCount: 0,
-  //   started: false,
-  //   startTime: null,
-  //   wordsPerMinute: null
-  // } 
-
-  // inputfield: string;
-  
-  // wordsPerMinute (charsTyped: number, ms: number): number {
-  //   return Math.floor((charsTyped / 5) / (ms / 60000))
-  // }
-  
-  // onWordChange(): void {
-  //   let e = this.inputfield
-  //   if (!this.state.started) {
-  //     this.state.started= true
-  //     this.state.startTime = new Date() 
-  //   }
-  //   console.log(e)
-  //   const enteredText = e.trim()
-  //   this.state.enteredText = enteredText
-  //   if (enteredText === this.state.words[0]) {
-  //     this.state.correctCount = this.state.correctCount + 1
-  //     this.state.enteredText = '';
-  //     this.state.words = this.state.words.slice(1)
-  //     this.checkIfFinished()
-  //   }
-  // }
-  
-    
-
-  // checkIfFinished(): void {
-  //   console.log(this.state.words.length)
-  //   if (!this.state.words.length) {
-  //     if (this.state.startTime) {
-  //       const timeMillis: number = new Date().getTime() - this.state.startTime.getTime()
-  //       const wpm = this.wordsPerMinute(this.testmat.length, timeMillis)
-  //       this.state.wordsPerMinute = wpm;
-  //     }
-  //   }
-  // }
