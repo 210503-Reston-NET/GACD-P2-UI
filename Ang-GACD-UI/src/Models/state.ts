11d7b2ceac97d740a0d39fc380b8d97c804434f8
phobos42 <garrettruffner42@gmail.com>
2021-06-12 03:53:01 -0400

    Typeing test is working if you dont use backspace :)

--- Ang-GACD-UI/src/Models/state.ts
+++ Ang-GACD-UI/src/Models/state.ts
@@ -1,8 +1,14 @@
 export interface State {
     words: string;
+    wordarray: Array<string[]>;
+    typedarray: Array<string[]>;
     enteredText: string;
     errors: number;
     started: boolean;
+    finished: boolean;
     startTime: Date;
     timeTaken: number;
+    letterPosition: number;
+    wordPosition: number;
+    correctchars: number;
   }
