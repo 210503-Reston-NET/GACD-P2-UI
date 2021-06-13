641d16e26576d50db73b3096e0e47aa55c08626c
phobos42 <garrettruffner42@gmail.com>
2021-06-13 05:58:10 -0400

    Big navbar update

--- Ang-GACD-UI/src/app/components/nav-bar/nav-bar.component.ts
+++ Ang-GACD-UI/src/app/components/nav-bar/nav-bar.component.ts
@@ -3,10 +3,12 @@ import { AppComponent } from 'src/app/app.component';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styles: [
+  styleUrls: [
+    './nav-bar.component.css'
   ]
 })
 export class NavBarComponent implements OnInit {
+  isCollapsed = false;
   loadingImg =
   'https://api.freelogodesign.org/files/4981cd4ed1774227824e712939d53c78/thumb/logo_200x200.png?v=637588683320000000';
   constructor() {    
--- Ang-GACD-UI/src/app/components/nav-buttons/login-button/login-button.component.ts
+++ Ang-GACD-UI/src/app/components/nav-buttons/login-button/login-button.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-login-button',
   templateUrl: './login-button.component.html',
-  styles: [
+  styleUrls: [ 
   ]
 })
 export class LoginButtonComponent implements OnInit {
