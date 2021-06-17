5f7d0740c8e034ae725e29f105d1ef8ea3a2fa80
daustindev <daustindev@gmail.com>
2021-06-17 10:51:54 -0500

    removed bullets from navbar

--- Ang-GACD-UI/src/app/create-competition/create-competition.component.spec.ts
+++ Ang-GACD-UI/src/app/create-competition/create-competition.component.spec.ts
@@ -1,10 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CompModel } from 'src/Models/CompModel';
 
 import { CreateCompetitionComponent } from './create-competition.component';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { HttpClient } from '@angular/common/http';
+import { RestService } from 'src/Services/rest.service';
+import { UserNameModel } from 'src/Models/UserNameModel';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { LangSelectComponent } from 'src/app/components/lang-select/lang-select.component';
+import { SnackBarComponent } from '../components/snack-bar/snack-bar.component';
+import { DatePipe } from '@angular/common';
 describe('CreateCompetitionComponent', () => {
   let component: CreateCompetitionComponent;
   let fixture: ComponentFixture<CreateCompetitionComponent>;
+  let newComp: CompModel;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,4 +32,7 @@ describe('CreateCompetitionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should create model', () => {
+    expect(newComp).toBeTruthy();
+  })
 });
