b649ef290846c2021d4e07b5c0679452b5a70e27
daustindev <daustindev@gmail.com>
2021-06-17 14:09:28 -0500

    worked on testing the addCompetition

--- Ang-GACD-UI/src/app/create-competition/create-competition.component.spec.ts
+++ Ang-GACD-UI/src/app/create-competition/create-competition.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CompModel } from 'src/Models/CompModel';
-
 import { CreateCompetitionComponent } from './create-competition.component';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
@@ -11,14 +10,24 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { LangSelectComponent } from 'src/app/components/lang-select/lang-select.component';
 import { SnackBarComponent } from '../components/snack-bar/snack-bar.component';
 import { DatePipe } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 describe('CreateCompetitionComponent', () => {
   let component: CreateCompetitionComponent;
   let fixture: ComponentFixture<CreateCompetitionComponent>;
   let newComp: CompModel;
+  let MockAuthService = {} as AuthService
 
+  class MockRestService
+  {
+    getTestContentByCatagoryId(){};
+    getloggedInUser(){};
+    postCompetition(){};
+  }
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateCompetitionComponent ]
+      imports:[RouterTestingModule, SnackBarComponent, DatePipe],
+      declarations: [ CreateCompetitionComponent ],
+      providers: [{provide: RestService, useClass: MockRestService}, {provide: AuthService, useClass: MockAuthService} ],
     })
     .compileComponents();
   });
@@ -32,7 +41,10 @@ describe('CreateCompetitionComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  // test comp model creation 
   it('should create model', () => {
     expect(newComp).toBeTruthy();
   })
+  //newComp properties should be set properly
 });
