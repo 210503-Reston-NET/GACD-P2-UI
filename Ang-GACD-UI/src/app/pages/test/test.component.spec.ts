a12950f2746c95a27cd38430bfe2e327c88db026
daustindev <daustindev@gmail.com>
2021-06-17 10:23:09 -0500

    found the test issue

--- Ang-GACD-UI/src/app/pages/test/test.component.spec.ts
+++ Ang-GACD-UI/src/app/pages/test/test.component.spec.ts
@@ -1,11 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TestComponent } from './test.component';
-
+import { RestService } from 'src/Services/rest.service';
+import { Usermodel } from 'src/Models/UserModel';
+import {UserService} from 'src/Services/User.service';
+import { ComponentFixtureAutoDetect }  from '@angular/core/testing';
+import { LangSelectComponent } from 'src/app/components/lang-select/lang-select.component';
+import { Language } from 'src/Models/LanguageEnum';
+import {Router} from "@angular/router";
+//import { Interface } from 'readline';
+import { templateJitUrl } from '@angular/compiler';
 describe('TestComponent', () => {
   let component: TestComponent;
   let fixture: ComponentFixture<TestComponent>;
-
+  let testString: string;
+  let inputString: string;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ TestComponent ]
@@ -22,4 +31,5 @@ describe('TestComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
