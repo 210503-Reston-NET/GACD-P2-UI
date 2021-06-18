eec3bd52e1c9b75ee8206ef471562ba6eb17b172
phobos42 <garrettruffner42@gmail.com>
2021-06-17 22:15:00 -0400

    tests

--- Ang-GACD-UI/src/app/pages/competition-result/competition-result.component.spec.ts
+++ Ang-GACD-UI/src/app/pages/competition-result/competition-result.component.spec.ts
@@ -1,16 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RestService } from 'src/Services/rest.service';
 
 import { CompetitionResultComponent } from './competition-result.component';
 
 describe('CompetitionResultComponent', () => {
+  class MockRestService
+  {
+    getCompetitionResults(): Promise<any>{
+      return new Promise<void>((resolve,reject)=> {});
+    }
+    //put api calls here if you want to test them
+  }
+
   let component: CompetitionResultComponent;
   let fixture: ComponentFixture<CompetitionResultComponent>;
+  let router: Router;
+  let rest: RestService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CompetitionResultComponent ]
+      declarations: [ CompetitionResultComponent ],
+      providers: [        
+        {provide: ActivatedRoute ,useValue: {id: 0}},
+        {provide: RestService, useClass: MockRestService},
+        {provide: ActivatedRoute,useValue: {id: 0}},
+                
+      ],
+      imports: [
+        RouterTestingModule,
+      ]
     })
     .compileComponents();
+    router = TestBed.inject(Router);
+    rest = TestBed.inject(RestService);
   });
 
   beforeEach(() => {
